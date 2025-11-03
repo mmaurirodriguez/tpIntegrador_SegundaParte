a2c8cfb964bfbac5e397db45c789aa36c6d6d98a
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-11-03 18:18:40 -0300

    agregue cosas al principio

---
+++ tpIntegrador/src/components/HomeMenu.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
---
+++ tpIntegrador/src/screens/Register.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
+//siemore ajustar esta ruta para que lleve a la confug de firebase
+import { auth, db} from '../firebase/config';
