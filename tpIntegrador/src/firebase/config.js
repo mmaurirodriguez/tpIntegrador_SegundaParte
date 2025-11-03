585a2d991e710adbe0b61117030793a2776f6987
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-11-03 18:06:45 -0300

    carpetas nuevas

---
+++ tpIntegrador/src/firebase/config.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQd4LayJcSj7gj47bVvSD1CpBQNGzX6BA",
+  authDomain: "proyecto-integrador-7a6fb.firebaseapp.com",
+  projectId: "proyecto-integrador-7a6fb",
+  storageBucket: "proyecto-integrador-7a6fb.firebasestorage.app",
+  messagingSenderId: "930496816309",
+  appId: "1:930496816309:web:6b20d15f2834b20acccafc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const storage = app.storage();
+export const db = app.firestore();
