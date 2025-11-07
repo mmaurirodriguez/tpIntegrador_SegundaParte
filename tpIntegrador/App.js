cdc71656057bf31893d68d1e09a2c44482c89983
AgusJara <ajaralambides@udesa.edu.ar>
2025-11-07 11:57:36 -0300

    Creo que termine lo de recordame, hay que chequear que funcione pero necesitamos el proyecto mas terminado

--- tpIntegrador/App.js
+++ tpIntegrador/App.js
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { auth } from './src/firebase/config';
 import Login from './src/screens/Login';
+import { Component } from 'react';
 const Stack = createNativeStackNavigator()
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logged: false,
+      loading: true
+    }
+  }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={ Login } options={{headerShown:false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          logged: true,
+          loading: false
+        })
+      } else {
+        this.setState({
+          logged: false,
+          loading: false
+        })
+      }
+    })
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        {this.state.loading ? (<ActivityIndicator size='large' color='green' />)
+          : (
+            <Stack.Navigator>
+              {this.state.logged ?
+                (<Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />) :
+                (<Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />)}
+            </Stack.Navigator>
+          )}
+      </NavigationContainer>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -25,3 +57,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
